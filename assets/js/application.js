c22cb3ddacde463690e5fadbf53df3105687c81e
Jonathan Milam Walters <jonathan@revelry.co>
2020-07-16 18:53:18 -0600

    Restore application of drop shadow to top menu

--- assets/js/application.js
+++ assets/js/application.js
@@ -23,30 +23,7 @@ jQuery(document).ready(function($){
     e.preventDefault();
   });
 
-  // Bind to scroll
-  $(window).scroll(function(){
-     // Get container scroll position
-     var fromTop = $(this).scrollTop()+topMenuHeight;
-
-     // Get id of current scroll item
-     var cur = scrollItems.map(function(){
-       if ($(this).offset().top < fromTop)
-         return this;
-     });
-     // Get the id of the current element
-     cur = cur[cur.length-1];
-     var id = cur && cur.length ? cur[0].id : "";
-
-     if (lastId !== id) {
-         lastId = id;
-         // Set/remove active class
-         menuItems
-           .parent().removeClass("active")
-           .end().filter("[href=#"+id+"]").parent().addClass("active");
-     }
-  });
-
-  // Animate menu on scroll
+  // Apply a drop shadow to top menu on scroll.
   $('body')
     .visibility({
       once: false,
